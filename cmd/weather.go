ea0700c2081dca11feeda4894ad1515166527a1f
EwanGreer <ewanja.greer@gmail.com>
2024-12-09 20:59:59 +0000

    feat: drop location flags scope

--- cmd/weather.go
+++ cmd/weather.go
@@ -10,14 +10,18 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var location = "belfast"
-
 // weatherCmd represents the weather command
 var weatherCmd = &cobra.Command{
 	Use:   "weather",
 	Short: "Gets the weather for belfast",
 	Long:  `Gets the weather for belfast from wttr.in in ANSI format`,
 	Run: func(cmd *cobra.Command, args []string) {
+		location, err := cmd.Flags().GetString("location")
+		if err != nil {
+			log.Println(err)
+			os.Exit(1)
+		}
+
 		resp, err := http.Get(fmt.Sprintf("https://wttr.in/%s?A", location)) // /A return ANSI format
 		if err != nil {
 			log.Println(err)
@@ -38,5 +42,5 @@ var weatherCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(weatherCmd)
 
-	weatherCmd.Flags().StringVarP(&location, "location", "l", "belfast", "The location to be queried for weather data")
+	weatherCmd.Flags().StringP("location", "l", "belfast", "The location to be queried for weather data")
 }
