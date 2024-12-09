6076c28db5664b956993525e53b4d7d614fc0d8e
EwanGreer <ewanja.greer@gmail.com>
2024-12-09 19:09:40 +0000

    feat: flags

--- cmd/weather.go
+++ cmd/weather.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
@@ -13,13 +10,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var location = "belfast"
+
 // weatherCmd represents the weather command
 var weatherCmd = &cobra.Command{
 	Use:   "weather",
 	Short: "Gets the weather for belfast",
-	Long:  `Gets the weather for belfast from wttr.in using format 3`,
+	Long:  `Gets the weather for belfast from wttr.in in ANSI format`,
 	Run: func(cmd *cobra.Command, args []string) {
-		resp, err := http.Get("https://wttr.in/belfast?A") // /A return ANSI format
+		resp, err := http.Get(fmt.Sprintf("https://wttr.in/%s?A", location)) // /A return ANSI format
 		if err != nil {
 			log.Println(err)
 			os.Exit(1)
@@ -39,13 +38,5 @@ var weatherCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(weatherCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// weatherCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// weatherCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	weatherCmd.Flags().StringVarP(&location, "location", "l", "belfast", "The location to be queried for weather data")
 }
